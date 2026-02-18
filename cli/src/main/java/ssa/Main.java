8d2579e152deb787f64f3fef2591235de775f2c2
obutr <butrym@karazin.ua>
2026-02-18 19:12:35 +0200

    Add basic quiz functionality to Main class

--- cli/src/main/java/ssa/Main.java
+++ cli/src/main/java/ssa/Main.java
@@ -1,3 +1,16 @@
 void main() {
-    IO.println("Hello World!");
+    Scanner scan = new Scanner(System.in);
+    System.out.println("What is JVM?");
+    System.out.println("""
+            1) Java Virtual Machine
+            2) Java
+            3) Java Platform
+            4) Just Virtual Memory
+            """);
+    String resp = scan.nextLine();
+    if (resp.equals("1") ) {
+        System.out.println("Correct!");
+    } else {
+        System.out.println("Wrong!");
+    }
 }
